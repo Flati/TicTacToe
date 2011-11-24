4f5e5f3f2c3fcdd403076489da9cb40570cdb357
Petur Haukur Johannesson <pesinn@gmail.com>
2011-11-24 15:14:47 +0000

    Logic changes

--- TicTacToe/TicTacToeApplication/Test/TestLogic.cs
+++ TicTacToe/TicTacToeApplication/Test/TestLogic.cs
@@ -34,7 +34,6 @@ namespace Test
                     Assert.AreNotEqual(logic.matrix[i][j], "Null");
                 }
             }
-
         }
         #region palli
         #endregion
@@ -68,7 +67,271 @@ namespace Test
         #endregion
 
         #region petur
+        [Test]
+        public void TestIsWonInTopHorizonalPositionForX()
+        {
+            // 1. Arrange:
+            Logic logic = new Logic();
 
+            // 2. Act:
+            logic.matrix[0][0] = "X";
+            logic.matrix[0][1] = "X";
+            logic.matrix[0][2] = "X";
+            logic.isWon();
+           
+            // 3. Assert:
+            Assert.AreEqual(logic.isWon(), true);
+        }
+
+        [Test]
+        public void TestIsWonInMidHorizonalPositionForX()
+        {
+            // 1. Arrange:
+            Logic logic = new Logic();
+
+            // 2. Act:
+            logic.isWon();
+            logic.matrix[1][0] = "X";
+            logic.matrix[1][1] = "X";
+            logic.matrix[1][2] = "X";
+            logic.isWon();
+            
+            //3. Assert:
+            Assert.AreEqual(logic.isWon(), true);
+        }
+
+        [Test]
+        public void TestIsWonInBottomHorizonalPositionForX()
+        {
+            // 1. Arrange:
+            Logic logic = new Logic();
+
+            // 2. Act:
+            logic.matrix[2][0] = "X";
+            logic.matrix[2][1] = "X";
+            logic.matrix[2][2] = "X";
+            logic.isWon();
+
+            //3. Assert:
+            Assert.AreEqual(logic.isWon(), true);
+        }
+
+        [Test]
+        public void TestIsWonInLeftVerticalPositionForX()
+        {
+            // 1. Arrange:
+            Logic logic = new Logic();
+
+            // 2. Act:
+            logic.matrix[0][0] = "X";
+            logic.matrix[1][0] = "X";
+            logic.matrix[2][0] = "X";
+            logic.isWon();
+
+            //3. Assert:
+            Assert.AreEqual(logic.isWon(), true);
+        }
+
+        [Test]
+        public void TestIsWonInMidVerticalPositionForX()
+        {
+            // 1. Arrange:
+            Logic logic = new Logic();
+
+            // 2. Act:
+            logic.matrix[1][0] = "X";
+            logic.matrix[1][1] = "X";
+            logic.matrix[1][2] = "X";
+            logic.isWon();
+
+            //3. Assert:
+            Assert.AreEqual(logic.isWon(), true);
+        }
+
+        [Test]
+        public void TestIsWonInRightVerticalPositionForX()
+        {
+            // 1. Arrange:
+            Logic logic = new Logic();
+
+            // 2. Act:
+            logic.matrix[2][0] = "X";
+            logic.matrix[2][1] = "X";
+            logic.matrix[2][2] = "X";
+            logic.isWon();
+
+            //3. Assert:
+            Assert.AreEqual(logic.isWon(), true);
+        }
+
+        [Test]
+        public void TestIsWonInDiagonalPositionForXTopLeftToBottomRight()
+        {
+            // 1. Arrange:
+            Logic logic = new Logic();
+
+            // 2. Act:
+            logic.matrix[0][0] = "X";
+            logic.matrix[1][1] = "X";
+            logic.matrix[2][2] = "X";
+            logic.isWon();
+
+            //3. Assert:
+            Assert.AreEqual(logic.isWon(), true);
+        }
+
+        [Test]
+        public void TestIsWonInDiagonalPositionForXBottomLeftToTopRight()
+        {
+            // 1. Arrange:
+            Logic logic = new Logic();
+
+            // 2. Act:
+            logic.matrix[2][2] = "X";
+            logic.matrix[1][1] = "X";
+            logic.matrix[0][0] = "X";
+            logic.isWon();
+
+            //3. Assert:
+            Assert.AreEqual(logic.isWon(), true);
+        }
+
+        [Test]
+        public void TestIsWonInTopHorizonalPositionForO()
+        {
+            // 1. Arrange:
+            Logic logic = new Logic();
+
+            // 2. Act:
+            logic.isWon();
+            logic.matrix[0][0] = "O";
+            logic.matrix[0][1] = "O";
+            logic.matrix[0][2] = "O";
+            logic.isWon();
+
+            //3. Assert:
+            Assert.AreEqual(logic.isWon(), true);
+        }
+
+
+        [Test]
+        public void TestIsWonInMidHorizonalPositionForO()
+        {
+            // 1. Arrange:
+            Logic logic = new Logic();
+
+            // 2. Act:
+            logic.isWon();
+            logic.matrix[1][0] = "O";
+            logic.matrix[1][1] = "O";
+            logic.matrix[1][2] = "O";
+            logic.isWon();
+
+            //3. Assert:
+            Assert.AreEqual(logic.isWon(), true);
+        }
+
+        [Test]
+        public void TestIsWonInBottomHorizonalPositionForO()
+        {
+            // 1. Arrange:
+            Logic logic = new Logic();
+
+            // 2. Act:
+            logic.matrix[2][0] = "O";
+            logic.matrix[2][1] = "O";
+            logic.matrix[2][2] = "O";
+            logic.isWon();
+
+            //3. Assert:
+            Assert.AreEqual(logic.isWon(), true);
+        }
+
+        [Test]
+        public void TestIsWonInLeftVerticalPositionForO()
+        {
+            // 1. Arrange:
+            Logic logic = new Logic();
+
+            // 2. Act:
+            logic.matrix[0][0] = "O";
+            logic.matrix[1][0] = "O";
+            logic.matrix[2][0] = "O";
+            logic.isWon();
+
+            //3. Assert:
+            Assert.AreEqual(logic.isWon(), true);
+        }
+
+        [Test]
+        public void TestIsWonInMidVerticalPositionForO()
+        {
+            // 1. Arrange:
+            Logic logic = new Logic();
+
+            // 2. Act:
+            logic.matrix[1][0] = "O";
+            logic.matrix[1][1] = "O";
+            logic.matrix[1][2] = "O";
+            logic.isWon();
+
+            //3. Assert:
+            Assert.AreEqual(logic.isWon(), true);
+        }
+
+        [Test]
+        public void TestIsWonInRightVerticalPositionForO()
+        {
+            // 1. Arrange:
+            Logic logic = new Logic();
+
+            // 2. Act:
+            logic.matrix[2][0] = "O";
+            logic.matrix[2][1] = "O";
+            logic.matrix[2][2] = "O";
+            logic.isWon();
+
+            //3. Assert:
+            Assert.AreEqual(logic.isWon(), true);
+        }
+
+        [Test]
+        public void TestIsWonInDiagonalPositionForOTopLeftToBottomRight()
+        {
+            // 1. Arrange:
+            Logic logic = new Logic();
+
+            // 2. Act:
+            logic.matrix[0][0] = "O";
+            logic.matrix[1][1] = "O";
+            logic.matrix[2][2] = "O";
+            logic.isWon();
+
+            //3. Assert:
+            Assert.AreEqual(logic.isWon(), true);
+        }
+
+        [Test]
+        public void TestIsWonInDiagonalPositionForOBottomLeftToTopRight()
+        {
+            // 1. Arrange:
+            Logic logic = new Logic();
+
+            // 2. Act:
+            logic.matrix[2][2] = "O";
+            logic.matrix[1][1] = "O";
+            logic.matrix[0][0] = "O";
+            logic.isWon();
+
+            //3. Assert:
+            Assert.AreEqual(logic.isWon(), true);
+        }
+
+        [Test]
+        public void TestIsWonForTenRandomGamesWhereThereIsATie()
+        {
+            
+        }
 
         #endregion
 
