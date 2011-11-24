5fcf07f3a43314a9b109557344a0706ac5f750de
Haflidi Orn Olafsson <flatmundur@gmail.com>
2011-11-24 11:59:41 +0000

    Looks for the window, Buttons with empty click functions

--- TicTacToe/TicTacToeApplication/TicTacToeApplication/Form1.cs
+++ TicTacToe/TicTacToeApplication/TicTacToeApplication/Form1.cs
@@ -15,5 +15,50 @@ namespace TicTacToeApplication
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
