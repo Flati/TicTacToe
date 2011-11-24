722f7193472f0f8b3cc21143492a49ba1b1cf6f9
Haflidi Orn Olafsson <flatmundur@gmail.com>
2011-11-24 18:12:53 +0000

    Breytt virkni a data reader. Lestur faerdur i constructor

--- TicTacToe/TicTacToeApplication/TicTacToeApplication/code/DataReader.cs
+++ TicTacToe/TicTacToeApplication/TicTacToeApplication/code/DataReader.cs
@@ -9,7 +9,8 @@ namespace TicTacToeApplication.code
     public class DataReader
     {
         public string path = "ProgressLog.txt";
-        public void Winning(string winner)
+
+        public DataReader()
         {
             if (File.Exists(path))
             {
@@ -26,20 +27,18 @@ namespace TicTacToeApplication.code
                 Global.PLAYER2WIN = 0;
                 Global.TIENUMBER = 0;
             }
+        }
+
+        public void Winning(string winner)
+        {
+            if (winner == Global.PLAYER1)
+                Global.PLAYER1WIN++;
+            else if (winner == Global.PLAYER2)
+                Global.PLAYER2WIN++;
+            else
+                Global.TIENUMBER++;
             using (StreamWriter sw = new StreamWriter(path))
             {
-                if (winner == Global.PLAYER1)
-                {
-                    Global.PLAYER1WIN++;
-                }
-                else if (winner == Global.PLAYER2)
-                {
-                    Global.PLAYER2WIN++;
-                }
-                else
-                {
-                    Global.TIENUMBER++;
-                }
                 sw.WriteLine(Global.PLAYER1WIN);
                 sw.WriteLine(Global.PLAYER2WIN);
                 sw.WriteLine(Global.TIENUMBER);
