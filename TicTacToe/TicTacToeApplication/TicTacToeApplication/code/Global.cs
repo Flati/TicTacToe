eb734fcf803a8c38b3cd465d0e72b5ce43f662a4
Haflidi Orn Olafsson <flatmundur@gmail.com>
2011-11-24 12:54:08 +0000

    Global class added

---
+++ TicTacToe/TicTacToeApplication/TicTacToeApplication/code/Global.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TicTacToeApplication.code
+{
+    class Global
+    {
+        public static string PLAYER1 = "X";
+        public static string PLAYER2 = "O";
+        public static string winMessage = "";
+    }
+}
